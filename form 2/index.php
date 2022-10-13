debbb1b4673516ff224e50bfe45d4306e393beaf
Ramsamy Rodrigue <210427@CCM-Asty-Moulin.local>
2022-10-13 14:29:09 +0200

    Commencement du formulaire 2

--- form 2/index.php
+++ form 2/index.php
@@ -7,6 +7,40 @@
     <title>Ramsamy_Ewan</title>
 </head>
 <body>
-    
+    <form action="" method="">
+        <fieldset>
+            <legend>Données Personnelles</legend>
+            <div>
+                <label for="prenom"> Entrez votre prenom : </label>
+                <input name="prenom" type="text" id="prenom">
+            </div>
+            <div>
+                <label for="nom"> Entrez votre nom : </label>
+                <input name="nom" type="text" id="nom">
+            </div>
+            <div>
+                <label for="naissance"> Date de naissance : </label>
+                <input name="naissance" type="date" id="naissance">
+            </div>
+        </fieldset>
+        <fieldset>
+            <legend>Les nouveux Input</legend>
+            <label for="URL">Votre URL : </label>
+            <label for="nom">Votre numéro de téléphone : </label>
+            <label for="nom">Votre appréciation (entre 10 et 20) : </label>
+            <label for="nom">Votre couleur préférée : </label>
+            <label for="nom">Votre recherche : </label>
+            <label for="nom">Choisir un fichier : </label>
+            <label for="nom">Choisir une heure : </label>
+            <label for="nom">Choisir un mois : </label>
+            <label for="nom">Choisir une semaine : </label>
+            <label for="nom">Vos explications : </label>
+            <label for="nom">Votre recherche : </label>
+        </fieldset>
+        <fieldset>
+            <legend>Bouton Envoie</legend>
+            <input type="submit" value="Envoyer">
+        </fieldset>
+    </form>
 </body>
 </html>
